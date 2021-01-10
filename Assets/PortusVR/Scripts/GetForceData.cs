de37b4aa887d4f650aa122eabbea9d5dd0dbcdb4
Илья Писарский <idpisarskiy@miem.hse.ru>
2021-01-10 18:35:29 +0300

    levertest1

---
+++ Assets/PortusVR/Scripts/GetForceData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GetForceData : MonoBehaviour
+{
+    public Joint JointTest;
+    public Text txt;
+    //public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //JointTest.connectedBody = rb;
+        txt.text = "Force " + JointTest.currentForce.magnitude;
+        Debug.Log("Force " + JointTest.currentForce);
+    }
+}
