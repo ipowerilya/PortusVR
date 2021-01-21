3005fe3d7b7dcd5dad75030f5cad57cf68156303
Алексей Рыжов <arryzhov@miem.hse.ru>
2021-01-21 19:17:53 +0300

    Add Weight.cs

---
+++ Assets/PortusVR/Scripts/Task/Lab1/Weight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Weight : ScoreCalculator
+{
+    public Joint joint;
+    public float targetForce;
+
+    public override float CalculateScore()
+    {
+        float force = joint.currentForce.magnitude;
+        float diff = Mathf.Abs(force - targetForce);
+        return diff < 1.0f
+               ? 5
+               : diff < 2.0f
+               ? 4
+               : diff < 3.0f
+               ? 3
+               : 2;
+    }
+}
