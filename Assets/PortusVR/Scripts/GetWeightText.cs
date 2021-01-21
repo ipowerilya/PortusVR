519305e6a2e5911bd871ab811480794fbbca67a3
Илья Писарский <idpisarskiy@miem.hse.ru>
2021-01-21 20:36:11 +0300

    Added models of lab equipment, polished scene

---
+++ Assets/PortusVR/Scripts/GetWeightText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetWeightText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (Text text in this.GetComponentsInChildren<Text>())
+        {
+            text.text = this.GetComponent<Rigidbody>().mass.ToString();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
