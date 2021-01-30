7a14b7314fd2efaee0b0ac71768ddad7b40019ed
Цыгикато Пётр <pntsygikalo@miem.hse.ru>
2021-01-30 17:07:59 +0300

    scene changes experiments

---
+++ Assets/PortusVR/Scripts/Invisible.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invisible : MonoBehaviour
+{
+    void Start()
+    {
+        foreach (var render in GetComponentsInChildren<Renderer>())
+        {
+            render.material.renderQueue = 2002;
+        }
+    }
+}
---
+++ Assets/PortusVR/Scripts/ResetScene.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ResetScene : MonoBehaviour
+{
+    public GameObject[] preservedObjects;
+    // Start is called before the first frame update
+
+    public void ResetSceneFunc()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+    }
+
+    /* public void ResetSceneFunc()
+    {
+        foreach (var obj in preservedObjects) 
+        {
+            DontDestroyOnLoad(obj);
+        }
+        StartCoroutine(AsyncLoadScene());
+    }*/
+
+    /*    Nye Rabotayet
+        IEnumerator AsyncLoadScene()
+        {
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+            while (!asyncLoad.isDone)
+            {
+                yield return null;
+            }
+
+            var currentScene = SceneManager.GetActiveScene();
+            foreach (var obj in preservedObjects)
+            {
+                SceneManager.MoveGameObjectToScene(obj, currentScene);
+            }
+            SceneManager.UnloadSceneAsync(currentScene);
+        }*/
+}
