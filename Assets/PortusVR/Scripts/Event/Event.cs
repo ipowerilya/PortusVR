57da21f42cd3edd2fa6602d187be319bbe82aa15
Alexey Ryzhov <arryzhov@miem.hse.ru>
2021-04-11 20:15:59 +0300

    add event script

---
+++ Assets/PortusVR/Scripts/Event/Event.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class Vector3Event : UnityEvent<Vector3> { }
+
+[System.Serializable]
+public class FloatEvent : UnityEvent<float> { }
+
+[System.Serializable]
+public class StringEvent : UnityEvent<string> { }
