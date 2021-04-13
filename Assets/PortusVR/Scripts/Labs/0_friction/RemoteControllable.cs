4760865e4828eceed7069bc5ddcc63d840723632
Alexey Ryzhov <arryzhov@miem.hse.ru>
2021-04-13 23:29:44 +0300

    update friction scene

---
+++ Assets/PortusVR/Scripts/Labs/0_friction/RemoteControllable.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(ConstantForce))]
+public class RemoteControllable : MonoBehaviour {
+    ConstantForce cf;
+    public Vector3 maxForce = new Vector3(1, 0, 0);
+    public FloatEvent onForceChange;
+
+    Rigidbody rb;
+    public Vector3 rotationStateA = Vector3.zero;
+    public Vector3 rotationStateB = Vector3.right * 90;
+
+    Vector3 position;
+
+    public void Start()
+    {
+        cf = GetComponent<ConstantForce>();
+        cf.force = Vector3.zero;
+
+        rb = GetComponent<Rigidbody>();
+
+        position = transform.position;
+    }
+    public void SetForce(Vector3 newForce) {
+        cf.force = newForce;
+        onForceChange.Invoke(newForce.magnitude);
+    }
+
+    public void SetForcePercentage(float percentage)
+    {
+        SetForce(maxForce * ((percentage - 50)/50));
+    }
+
+    public void SetRotationState(bool rotationState)
+    {
+        var rotation = rotationState ? rotationStateB : rotationStateA;
+        rb.rotation = Quaternion.Euler(rotation);
+    }
+
+    public void SetRotationA()
+    {
+        SetRotationState(false);
+    }
+
+    public void SetRotationB()
+    {
+        SetRotationState(true);
+    }
+
+    public void ResetState()
+    {
+        transform.position = position;
+        cf.force = Vector3.zero;
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+}
---
+++ Assets/PortusVR/Scripts/Labs/0_friction/TextController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class TextController : MonoBehaviour
+{
+    Text text;
+
+    public void Start()
+    {
+        text = GetComponent<Text>();
+    }
+
+    public void SetFloat(float val)
+    {
+        text.text = val.ToString("000");
+    }
+}
