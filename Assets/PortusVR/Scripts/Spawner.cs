41b62bc1054728e3ef2fc00f65a5da8b87e07b89
Илья Писарский <idpisarskiy@miem.hse.ru>
2021-04-14 16:42:09 +0300

    Added experiment result table and main object instantiation in lab 4

--- Assets/PortusVR/Scripts/Spawner.cs
+++ Assets/PortusVR/Scripts/Spawner.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    
+    public Transform SpawnPoint;
+    public GameObject SpherePrefab;
+    public GameObject CurrentSphere;
+    public TimerData TimerObj;
     // Start is called before the first frame update
     void Start()
     {
         
+    }
+    public void CreatSphere()
+    {
+        CurrentSphere = Instantiate(SpherePrefab, SpawnPoint.position, SpawnPoint.rotation);
+        CurrentSphere.GetComponent<TimerScript>().TimerObj = TimerObj;
     }
 
     // Update is called once per frame
