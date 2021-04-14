bdf6f2e231094e69f9853fa9bd027218827c2404
Alexey Ryzhov <arryzhov@miem.hse.ru>
2021-04-14 19:57:24 +0300

    update 1_friction

--- Assets/PortusVR/Scripts/Labs/1_friction/RemoteControllable.cs
+++ Assets/PortusVR/Scripts/Labs/1_friction/RemoteControllable.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,45 +15,38 @@ public class RemoteControllable : MonoBehaviour {
     public Vector3 rotationStateB = Vector3.right * 90;
 
     Vector3 position;
-
-    public void Start()
-    {
+    
+    public void Awake() {
         cf = GetComponent<ConstantForce>();
         cf.force = Vector3.zero;
 
         rb = GetComponent<Rigidbody>();
-
-        position = transform.position;
+        position = transform.localPosition;
     }
     public void SetForce(Vector3 newForce) {
         cf.force = newForce;
         onForceChange.Invoke(newForce.magnitude);
     }
 
-    public void SetForcePercentage(float percentage)
-    {
-        SetForce(maxForce * ((percentage - 50)/50));
+    public void SetForcePercentage(float percentage) {
+        SetForce((maxForce * (percentage - 50f)*2f)/100f);
     }
 
-    public void SetRotationState(bool rotationState)
-    {
+    public void SetRotationState(bool rotationState) {
         var rotation = rotationState ? rotationStateB : rotationStateA;
         rb.rotation = Quaternion.Euler(rotation);
     }
 
-    public void SetRotationA()
-    {
+    public void SetRotationA() {
         SetRotationState(false);
     }
 
-    public void SetRotationB()
-    {
+    public void SetRotationB() {
         SetRotationState(true);
     }
 
-    public void ResetState()
-    {
-        transform.position = position;
+    public void ResetState() {
+        transform.localPosition = position;
         cf.force = Vector3.zero;
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
