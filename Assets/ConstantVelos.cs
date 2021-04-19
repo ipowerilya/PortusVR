5d4d49c047435c2ae9d2378fecfc1068c1f0d24f
Илья Писарский <idpisarskiy@miem.hse.ru>
2021-04-19 23:04:20 +0300

    commit fix

---
+++ Assets/ConstantVelos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantVelos : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ConstantVelocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(0,0, ConstantVelocity);
+    }
+}
