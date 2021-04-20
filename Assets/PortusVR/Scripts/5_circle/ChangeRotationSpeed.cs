3c33ca6e4afcb483a5d4ad6c2103b053fe455c11
Цыгикато Пётр <pntsygikalo@miem.hse.ru>
2021-04-20 18:23:39 +0300

    added cosmetics plans and rotation managment in lab 5

---
+++ Assets/PortusVR/Scripts/5_circle/ChangeRotationSpeed.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeRotationSpeed : MonoBehaviour
+{
+    //public HingeJoint hinge_joint;
+    public GameObject moving_point;
+    private HingeJoint hinge_joint;
+    // degrees per second
+    public float MaxTargetVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hinge_joint = moving_point.GetComponent<HingeJoint>();
+        //UpdateRotationSpeed(0);
+    }
+    public void UpdateRotationSpeed(float percentage_raw)
+    {
+        float percentage = ((percentage_raw / 100f) - 0.5f) * 2; // change interval from [0;1] to [-1;1]
+        var motor = hinge_joint.motor;
+        
+        motor.targetVelocity = percentage * MaxTargetVelocity;
+        hinge_joint.motor = motor;
+    }
+}
