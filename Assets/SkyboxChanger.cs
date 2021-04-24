e4f0d904c9c83bf0d4d46133cbbb43e91a32de44
Илья Писарский <idpisarskiy@miem.hse.ru>
2021-04-24 18:49:48 +0300

    added Skybox Changer

---
+++ Assets/SkyboxChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChanger : MonoBehaviour
+{
+    public Material[] Skybox;
+    // Start is called before the first frame update
+   
+    public void ChangeSkybox(int SkyboxNumber)
+    {
+        RenderSettings.skybox = Skybox[SkyboxNumber];
+    }
+ 
+}
