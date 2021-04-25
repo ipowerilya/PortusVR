8005716373bc8e3f9ae3bac58bada7a76e0e0252
Alexey Ryzhov <arryzhov@miem.hse.ru>
2021-04-25 21:38:16 +0300

    add object clipping trigger

---
+++ Assets/PortusVR/Scripts/8_hub/ClippingTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class ClippingTrigger : MonoBehaviour
+{
+    Collider thisCollider;
+    private void Start()
+    {
+        thisCollider = GetComponent<Collider>();
+        if (!thisCollider.isTrigger) throw new UnityException("Clipping collider must be a trigger");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
