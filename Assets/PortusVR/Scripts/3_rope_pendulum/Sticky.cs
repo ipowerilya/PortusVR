2871326619b4fe1fbaa12f20070be8d1497967d5
Alexey Ryzhov <arryzhov@miem.hse.ru>
2021-04-26 23:41:21 +0300

    add Sticky script

---
+++ Assets/PortusVR/Scripts/3_rope_pendulum/Sticky.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Sticky : MonoBehaviour
+{
+    public Collider target;
+    Joint joint;
+    float detachTime;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (target == collision.collider && (Time.time - detachTime) > 0.2f)
+        {
+            joint = gameObject.AddComponent<FixedJoint>();
+            joint.connectedBody = target.attachedRigidbody;
+        }
+    }
+
+    public void Detach()
+    {
+        detachTime = Time.time;
+        Destroy(joint);
+    }
+}
