f3c9a9fe8489dd27af9311eb90ca6ccc6c901093
Alexey Ryzhov <arryzhov@miem.hse.ru>
2021-04-30 21:00:34 +0300

    update lab 5

---
+++ Assets/PortusVR/Scripts/5_circle/ConstantRotation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ConstantForce))]
+public class ConstantRotation : MonoBehaviour
+{
+    public Transform axisPoint;
+    public float forceMagnitude { get; set; } = 1f;
+
+    Vector3 axis { get { return axisPoint.TransformVector(Vector3.up); } }
+    Vector3 toAxis { get { return axisPoint.position - transform.position; } }
+
+    Rigidbody rb;
+    ConstantForce cf;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+        cf = gameObject.GetComponent<ConstantForce>();
+    }
+
+    public Vector3 GetAxisOrtho()
+    {
+        return toAxis - axis * (Vector3.Dot(toAxis, axis) / Vector3.Dot(axis, axis));
+    }
+
+    public Vector3 GetAxisTangent()
+    {
+        return Vector3.Cross(axis, toAxis);
+    }
+
+    private void FixedUpdate()
+    {
+        var tangent = GetAxisTangent();
+        cf.force = tangent.normalized * forceMagnitude / (tangent.sqrMagnitude + 1);
+    }
+}
--- Assets/PortusVR/Scripts/5_circle/Lab5ReadRotationMetrics.cs
+++ Assets/PortusVR/Scripts/5_circle/Lab5ReadRotationMetrics.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Lab5ReadRotationMetrics : MetricReading
 {
+    public ConstantRotation cr;
     public Stopwatch stopwatch;
 
     public override void ReadMetric()
     {
-        var time = stopwatch.Time;
-        AddMetric("Время (с)", time);
+        var time = stopwatch.Time / 30.0f;
+        var radius = cr.GetAxisOrtho().magnitude;
+        var accel = 4 * Mathf.PI * Mathf.PI * radius / (time * time);
+        var mass = cr.GetComponent<Rigidbody>().mass;
+        var centrifugalForce = cr.GetAxisOrtho().normalized * mass * accel;
+        var mg = Physics.gravity * mass;
+        var tension = centrifugalForce - mg;
 
-        // AddMetric("Сила (Н)", force);
-        // AddMetric("Радиус (м)", radius);
-        // AddMetric("Центростремительное ускорение (М/c*2)", accel);
-        // AddMetric("Сила упругости (Н)", tension);
+        AddMetric("Время (с)", time);
+        AddMetric("Радиус (м)", radius);
+        AddMetric("Центр.уск.(М/c*2)", accel);
+        AddMetric("Сила (Н)", centrifugalForce.magnitude);
+        AddMetric("Сила упр. (Н)", tension.magnitude);
     }
 }
