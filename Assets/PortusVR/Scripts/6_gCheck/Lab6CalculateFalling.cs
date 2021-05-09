0e7a9a58b6c811ce4c8d5071624df7619aa19ef1
Цыгикато Пётр <pntsygikalo@miem.hse.ru>
2021-05-09 22:39:00 +0300

    fixed lab 6 and reworked terrain in lab 3, minor fixes

---
+++ Assets/PortusVR/Scripts/6_gCheck/Lab6CalculateFalling.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lab6CalculateFalling : MetricReading
+{
+    public Stopwatch stopwatch;
+    public Transform top;
+    public Transform bottom;
+
+    public override void ReadMetric()
+    {
+        var time = stopwatch.Time;
+        var height = Vector3.Distance(top.position, bottom.position);
+        var accel = 2 * height / time / time;
+        AddMetric("Время (с)", time);
+        AddMetric("Высота (м)", height);
+        AddMetric("Ускор. (м/с^2)", accel, true);
+    }
+}
