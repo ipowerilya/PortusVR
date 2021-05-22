d990dd1d234c2d3728eb58513546df5e36a8e852
Alexey Ryzhov <arryzhov@miem.hse.ru>
2021-05-22 12:33:29 +0300

    add description scrollbar

--- Assets/PortusVR/Scripts/0_lever/MomentText.cs
+++ Assets/PortusVR/Scripts/0_lever/MomentText.cs
@@ -17,6 +17,6 @@ public class MomentText : MonoBehaviour
             .Where(sz => sz.SnappedObjectJoint != null && sz.SnapToObject != null)
             .Select(sz => Vector3.Distance(sz.transform.position, pivot.position) * sz.SnappedObjectJoint.currentForce.magnitude)
             .Sum();
-        outputText.text = moment.ToString("0.00");
+        outputText.text = moment.ToString("0.00") + " Н*м";
     }
 }
